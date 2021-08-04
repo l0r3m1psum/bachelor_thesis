7278e58687b8179c5239a0e4b74e67546595c7d5
l0r3m1psum <bellanidiego@gmail.com>
2021-08-04 16:31:18 +0200

    Done some work on the schema.

--- src/schema.sql
+++ src/schema.sql
@@ -3,32 +3,40 @@
 \pset footer off
 \pset border 2
 \pset linestyle unicode
+\set db test
 drop database if exists :db;
 create database :db;
 \c :db
 
+-- TODO: should the boxes be or in our piece of earth?
+
 begin;
 create table map (
-	-- TODO: find a base for x and y to check 10 meter distance
-	x              integer  not null check (x >= 0 and (x - 4559505)%10 = 0),
-	y              integer  not null check (y >= 0 and (y - 2130995)%10 = 0),
-	altimetry      smallint not null check (altimetry between 0 and 4380),
-	forest         smallint not null check (forest between 0 and 2
-		or forest = 255),
-	urbanization   smallint not null check (urbanization between 0 and 255),
-	water1         smallint not null check (water1 between 0 and 4
-		or water1 = 253 or water1 = 255),
-	water2         boolean  not null,
-	"carta natura" smallint not null check ("carta natura" between 1 and 90),
-	primary key (x, y)
+	-- rectangle        box    primary key check (rectangle <@ box '(0, 0), (Infinity, Infinity)'), -- 32 bytes
+	x1 float8,
+	y1 float8,
+	x2 float8,
+	y2 float8,
+	primary key (x1, y1, x2, y2),
+	check (box(point(x1, y1), point(x2, y2)) <@ box '(0, 0), (Infinity, Infinity)'),
+	altimetry        int2   not null check (altimetry between 0 and 4380),
+	forest           int2   not null check (forest between 0 and 2 or forest = 255),
+	urbanization     int2   not null check (urbanization between 0 and 255),
+	water1           int2   not null check (water1 between 0 and 4 or water1 = 253 or water1 = 255),
+	water2           bool   not null,
+	"carta natura"   int2   not null check ("carta natura" between 1 and 90),
+	"wind direction" float8 not null check ("wind direction" between -pi() and pi()),
+	"wind speed"     float8 not null check ("wind speed" >= 0)
+	-- exclude using gist (rectangle with ?#)
 );
 
-create table meteo (
-	x integer not null,
-	y integer not null,
-	"wind speed" float8 not null,
-	"wind direction" float8 not null,
-	foreign key (x, y) references map(x, y)
+create table results (
+	timestamp timestamp not null, -- 8 bytes
+	rectangle box not null,
+	fuel float8 not null check (fuel >= 0),
+	state bool,
+	primary key (timestamp, rectangle),
+	exclude using gist (timestamp with =, rectangle with ?#)
 );
 
 commit;
