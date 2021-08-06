83fffb22dfb0d58c62b713c2774d5a9b028e0087
l0r3m1psum <bellanidiego@gmail.com>
2021-08-06 18:16:45 +0200

    Updated schema.

--- src/schema.sql
+++ src/schema.sql
@@ -12,13 +12,9 @@ create database :db;
 
 begin;
 create table map (
-	-- rectangle        box    primary key check (rectangle <@ box '(0, 0), (Infinity, Infinity)'), -- 32 bytes
-	x1 float8,
-	y1 float8,
-	x2 float8,
-	y2 float8,
-	primary key (x1, y1, x2, y2),
-	check (box(point(x1, y1), point(x2, y2)) <@ box '(0, 0), (Infinity, Infinity)'),
+	x                int4   check (x >= 0),
+	y                int4   check (x >= 0),
+	primary key (x, y),
 	altimetry        int2   not null check (altimetry between 0 and 4380),
 	forest           int2   not null check (forest between 0 and 2 or forest = 255),
 	urbanization     int2   not null check (urbanization between 0 and 255),
@@ -27,16 +23,15 @@ create table map (
 	"carta natura"   int2   not null check ("carta natura" between 1 and 90),
 	"wind direction" float8 not null check ("wind direction" between -pi() and pi()),
 	"wind speed"     float8 not null check ("wind speed" >= 0)
-	-- exclude using gist (rectangle with ?#)
 );
 
 create table results (
-	timestamp timestamp not null, -- 8 bytes
-	rectangle box not null,
-	fuel float8 not null check (fuel >= 0),
-	state bool,
-	primary key (timestamp, rectangle),
-	exclude using gist (timestamp with =, rectangle with ?#)
+	ts    timestamp, -- 8 bytes
+	x     int4  check (x >= 0),
+	y     int4  check (x >= 0),
+	primary key (ts, x, y),
+	fuel  float8 not null check (fuel >= 0),
+	state bool   not null,
 );
 
 commit;
