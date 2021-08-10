c76a39674f271d145f9d6963c99d5c9b8790ca5c
l0r3m1psum <bellanidiego@gmail.com>
2021-08-10 10:59:11 +0200

    Minor refactoring.

--- src/platform_query.c
+++ src/platform_query.c
@@ -1,5 +1,5 @@
 #define CPUID(func, ax, bx, cx, dx)                  \
-	asm volatile (                                   \
+	__asm__ volatile (                                   \
 		"cpuid" :                                    \
 		"=a" (ax), "=b" (bx), "=c" (cx), "=d" (dx) : \
 		"a" (func)                                   \
@@ -14,6 +14,39 @@
  * https://stackoverflow.com/questions/14283171/how-to-receive-l1-l2-l3-cache-size-using-cpuid-instruction-in-x86
  * https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=292
  */
+
+
+static void
+print_cache_info(uint32_t cache_level) {
+	assert(cache_level < 4);
+	uint32_t a, b, c, d;
+	asm volatile (
+		"mov %0, %%ecx\n"
+		"mov $4, %%eax\n"
+		"cpuid"
+		: "=a" (a), "=b" (b), "=c" (c), "=d" (d)
+		: "r" (cache_level)
+	);
+	uint32_t res = 0, S = 0;
+	asm volatile ("mov %%ebx, %0\nmov %%ecx, %1" : "=r" (res), "=r" (S));
+#define L_maks 0x000007ff /* [11:0] */
+#define P_mask 0x001ff800 /* [21:12] */
+#define W_mask 0xffe00000 /* [31:22] */
+	uint32_t
+		L =  res & L_maks,
+		P =  (res & P_mask) >> 12,
+		W =  (res & W_mask) >> 22;
+	static_assert(L_maks | P_mask | W_mask == 0xffffffff, "bad masks");
+#undef L_maks
+#undef P_maks
+#undef W_maks
+	printf("L%d\n", cache_level);
+	printf("Cache Size: %d\n", (W + 1) * (P + 1) * (L + 1) * (S + 1));
+	printf("System Coherency Line Size %d\n", L+1);
+	printf("Physical Line partitions %d\n", P+1);
+	printf("Ways of associativity %d\n", W+1);
+	printf("Number of Sets %d\n", S+1);
+}
 int
 main(void) {
 	uint32_t a, b, c, d;
@@ -39,31 +72,10 @@ main(void) {
 		printf("cache line size: %d\n", res*8);
 	}
 
-	{
-		asm volatile (
-			"mov $1, %%ecx\n"
-			"mov $4, %%eax\n"
-			"cpuid" :
-			"=a" (a), "=b" (b), "=c" (c), "=d" (d)
-		);
-		uint32_t res = 0, S = 0;
-		asm volatile ("mov %%ebx, %0\nmov %%ecx, %1" : "=r" (res), "=r" (S));
-#define L_maks 0x000007ff /* [11:0] */
-#define P_mask 0x001ff800 /* [21:12] */
-#define W_mask 0xffe00000 /* [31:22] */
-		uint32_t
-			L =  res & L_maks,
-			P =  (res & P_mask) >> 12,
-			W =  (res & W_mask) >> 22;
-		static_assert(L_maks | P_mask | W_mask == 0xffffffff, "bad masks");
-#undef L_maks
-#undef P_maks
-#undef W_maks
-		printf("System Coherency Line Size %d\n", L+1);
-		printf("Physical Line partitions %d\n", P+1);
-		printf("Ways of associativity %d\n", W+1);
-		printf("Number of Sets %d\n", S+1);
+	print_cache_info(0);
+	print_cache_info(1);
+	print_cache_info(2);
+	print_cache_info(3);
 
-	}
 	return 0;
 }
