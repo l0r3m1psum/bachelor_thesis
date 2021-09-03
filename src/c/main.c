45c32afd469e3e6c038f6b9dc2b29a6fd599e6ca
l0r3m1psum <bellanidiego@gmail.com>
2021-09-03 10:55:59 +0200

    Prototyped main.

--- src/c/main.c
+++ src/c/main.c
@@ -1,10 +1,41 @@
+/* Considerare "log" asincrono per salvare lo stato del simulatore. Questo
+ * potrebbe essere realizzato tramite un ring buffer nel quale il simulatore
+ * produce una sequenza di stati e il logger, con i suoi thread, li consuma
+ * (i.e. li salva sul disco). Qui l'hypertreading dovrebbe aiutare siccome
+ * logger e simulatore dovrebbero usare parti diverse della CPU.
+ *
+ * Fase di inizializzazione
+ *   - Leggere il csv dei parametri delle celle, validarlo, emettendo
+ *     eventuali warning o errori e trasformarli in quelli necessari alla
+ *     simulazione;
+ *   - leggere il csv dello stato iniziale e validarlo;
+ *   - leggere il csv dei parametri globali della simulazione e e validarlo;
+ *   - creare eventuali thread pool;
+ *   - allocare tutta la memoria necessaria al simulatore.
+ *
+ * Fase di esecuzione
+ *   - Durante questa fase, dopo aver lanciato il simulatore, mi devo solo
+ *     preoccupare di gestire il segnale per la terminazione "gentile" (i.e.
+ *     il programma deve finire di scrivere l'ultimo stato per intero)
+ *
+ * Fase di spegnimento
+ *  - Nulla di particolare è necessario in questa fase, se non liberare la
+ *    memoria, ma siccome il sistema operativo lo farà comunque alla fine
+ *    per noi non è realmente necassario.
+ */
+
 #include "csv.h"
+#include "simulator.h"
 #include <stdalign.h>
 #include <string.h> /* strerror */
-#include <inttypes.h>
+#include <inttypes.h> /* PRI macros */
+#include <signal.h>
+#include <syslog.h>
+#include <stdlib.h>
+#include <unistd.h> /* sleep */
 
-int
-main(int argc, char *argv[]) {
+static int
+old_main(int argc, char *argv[]) {
 	if (argc < 2) {
 		fprintf(stderr, "error: missing file name argument\n");
 		return 1;
@@ -41,4 +72,96 @@ main(int argc, char *argv[]) {
 	}
 	puts("finished successfully!");
 	csv_close(it);
+	return 0;
+}
+
+static bool
+dump(simulation_t *s) {
+	for (uint64_t i = 0; i < s->Lstar; i++) {
+		for (uint64_t j = 0; j < s->Wstar-1; j++) {
+			const uint64_t ij = i + j*s->Wstar;
+			printf("%d,", s->new_state[ij].N);
+		}
+		printf("%d\n", s->new_state[i + (s->Wstar-1)*s->Wstar].N);
+	}
+	putchar('\n');
+	sleep(2);
+	return true;
+}
+
+int
+main(int argc, char *argv[]) {
+	openlog(argv[0], LOG_PERROR, 0);
+
+	{
+		syslog(LOG_INFO, "setting up interuption hanling");
+		sigset_t set = {0};
+		struct sigaction action = {
+			.sa_handler = simulation_SIGTER_handler,
+			.sa_mask = (sigset_t){0},
+			.sa_flags = SA_RESTART
+		};
+		if (sigfillset(&set) == -1) {
+			syslog(LOG_WARNING, "unable to fill sigset");
+		}
+		if (sigdelset(&set, SIGTERM) == -1) {
+			syslog(LOG_WARNING, "unable to delete SIGTERM from sigset");
+		}
+		if (sigprocmask(SIG_SETMASK, &set, NULL) == -1) {
+			syslog(LOG_WARNING, "unable to set the sigprocmask");
+		}
+		if (sigaction(SIGTERM, &action, NULL) == -1) {
+			syslog(LOG_WARNING, "unable to set SIGTERM handler");
+		}
+	}
+
+	const uint64_t width = 10, height = 10, area = width * height;
+	simulation_t s = {
+		.Wstar = width,
+		.Lstar = height,
+		.h = 30,
+		.s = 1,
+		.seed = 123,
+		.tau = 1,
+		.theta = 0.4f,
+		.k0 = 1,
+		.k1 = 1,
+		.k2 = 1,
+		.L = 1,
+		.old_state = malloc(sizeof (state_t) * area),
+		.new_state = malloc(sizeof (state_t) * area),
+		.params = malloc(sizeof (params_t) * area),
+		.gamma = malloc(sizeof (float) * area)
+	};
+
+	if (!(s.old_state && s.new_state && s.params && s.gamma)) {
+		return EXIT_FAILURE;
+	}
+
+	for (uint64_t i = 0; i < area; i++) {
+		s.old_state[i] = (state_t){.B = 0.5f, .N = false};
+		s.new_state[i] = (state_t){0};
+		s.params[i] = (params_t){.P = 1, .S = 0.7f, .F = 1, .D = pi};
+		s.gamma[i] = 10;
+	}
+
+	for (uint64_t i = 0; i < s.Lstar; i++) {
+		for (uint64_t j = 0; j < s.Wstar-1; j++) {
+			if (3 <= i && i <= 5 && 3 <= j && j <= 5) {
+				uint64_t ij = i + j*s.Wstar;
+				s.old_state[ij] = (state_t){ .B = 10, .N = true };
+			}
+		}
+	}
+
+	simulation_run(&s, dump);
+
+	free(s.old_state);
+	free(s.new_state);
+	free(s.params);
+	free(s.gamma);
+
+	closelog();
+
+	return EXIT_SUCCESS;
 }
