8c851b1cb4f9345010b77d441243e52a119007e0
l0r3m1psum <bellanidiego@gmail.com>
2021-09-05 18:55:25 +0200

    Removed const qualifier from struct.

--- src/c/simulator.h
+++ src/c/simulator.h
@@ -28,20 +28,20 @@ static_assert(sizeof (params_t) == 16, "bad size");
 struct {
 	state_t * restrict old_state;
 	state_t * restrict new_state;
-	params_t * const params;
-	float * const gamma; /* all initial fuels */
-	const uint64_t Wstar; /* number of cells along the x axis */
-	const uint64_t Lstar; /* number of cells along the y axis */
-	const uint64_t h; /* horizon */
-	const uint64_t s; /* snapshot frequency */
-	const uint32_t seed;
+	params_t *params;
+	float *gamma; /* all initial fuels */
+	uint64_t Wstar; /* number of cells along the x axis */
+	uint64_t Lstar; /* number of cells along the y axis */
+	uint64_t h; /* horizon */
+	uint64_t s; /* snapshot frequency */
+	uint32_t seed;
 	/* 32bit padding */
-	const float tau; /* time step */
-	const float theta; /* ingnition threshold */
-	const float k0; /* optimization parameter */
-	const float k1; /* wind optimization */
-	const float k2; /* slope optimization */
-	const float L; /* length of the sides of the cells in meters */
+	float tau; /* time step */
+	float theta; /* ingnition threshold */
+	float k0; /* optimization parameter */
+	float k1; /* wind optimization */
+	float k2; /* slope optimization */
+	float L; /* length of the sides of the cells in meters */
 } typedef simulation_t;
 static_assert(sizeof (simulation_t) == 96, "bad size");
 
