d15235a1925bfd590d9a0090e9942e2e404a87f2
l0r3m1psum <bellanidiego@gmail.com>
2021-09-08 10:16:06 +0200

    Added comments and minor refactoring.

--- src/c/csv.c
+++ src/c/csv.c
@@ -6,13 +6,19 @@
 
 static const char field_sep = ',';
 
+/* @param str null terminated string
+ * @param num where the parsed number will be stored
+ * @param type how str shall be read
+ * @return true on successfull reading of the number else false
+ */
 static inline bool
-try_insert_num(const char *str, uint64_t where, csv_num *nums, const csv_type *types) {
+try_insert_num(const char *str, csv_num *num, csv_type type) {
+	assert(str && nums);
 	char *endptr = NULL;
-	if (types[where] == CSV_INT64) {
-		nums[where].integ = strtoll(str, &endptr, 10);
-	} else /* types[where] == CSV_DOUBLE */ {
-		nums[where].doubl = strtod(str, &endptr);
+	if (type == CSV_INT64) {
+		num->integ = strtoll(str, &endptr, 10);
+	} else /* type == CSV_DOUBLE */ {
+		num->doubl = strtod(str, &endptr);
 	}
 	if (str == endptr) {
 		syslog(LOG_ERR, "unable to parse number '%s'", str);
@@ -21,9 +27,15 @@ try_insert_num(const char *str, uint64_t where, csv_num *nums, const csv_type *t
 	return true;
 }
 
+/* @param row null terminated string
+ * @param len length of nums and types
+ * @param nums where the numbers read will be stored
+ * @param types instructions on how to read the numbers in row
+ * @return true on successfull reading of all the numbers else false
+ */
 bool
 csv_read(char *row, uint64_t len, csv_num *nums, const csv_type *types) {
-	assert(len && nums && types);
+	assert(row && len && nums && types);
 
 	for (uint64_t i = 0; i < len-1; i++) {
 		char *sep = strchr(row, field_sep);
@@ -32,7 +44,7 @@ csv_read(char *row, uint64_t len, csv_num *nums, const csv_type *types) {
 			return false;
 		}
 		*sep = '\0';
-		if (!try_insert_num(row, i, nums, types)) {
+		if (!try_insert_num(row, nums+i, types[i])) {
 			return false;
 		}
 		row = sep+1;
@@ -43,7 +55,8 @@ csv_read(char *row, uint64_t len, csv_num *nums, const csv_type *types) {
 		syslog(LOG_ERR, "there are more rows than necessary");
 		return false;
 	}
-	if (!try_insert_num(row, len-1, nums, types)) {
+	const uint64_t last = len-1;
+	if (!try_insert_num(row, nums+last, types[last])) {
 		return false;
 	}
 
