3d08c25548c5b8bcf43a93a260a93de5eda363bd
l0r3m1psum <bellanidiego@gmail.com>
2021-09-15 11:53:48 +0200

    Added some draft code.

--- src/sql/export.sh
+++ src/sql/export.sh
@@ -11,3 +11,37 @@ tbname="$2"
 fname="$3"
 
 psql -d "$simulator" -c "\\\\copy \"$table\" to '$file' with format csv;"
+
+exit 0
+
+select unnest(data[1:3][2:4]) from maps;
+select unnest(data[1:3][2:4]) from results;
+select
+	array_length(rect[0], 1) as Wstar,
+	array_length(rect, 2) as Lstar,
+	horizon as h,
+	"snapshot freq" as s
+	seed,
+	tau,
+	theta,
+	k0,
+	k1,
+	k2,
+	L,
+	Delta
+from simulations
+order by started desc
+fetch first row only;
+
+espressione per calcolare gamma
+4000*(
+	(case
+		when forest < 255 then forest + 1
+		else 0
+	end case)
+	-
+	(case
+		when urbanization <= 100 then urbanization/100
+		else 0
+	end case)
+)
