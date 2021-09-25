f79993c1549fbe0d89a8445e0d0094a6090c2c50
l0r3m1psum <bellanidiego@gmail.com>
2021-09-25 11:06:46 +0200

    Added comment of invariant.

--- src/c/simulator.c
+++ src/c/simulator.c
@@ -24,13 +24,13 @@ maxf(float a, float b) {
 #if OPTIMIZED
 static float
 f_diag(int8_t e1, int8_t e2, float alpha) {
-	assert((e1 == 1 || e1 == -1) && (e2 == 1 || e2 == -1));
+	assert((e1 == 1 || e1 == -1) && (e2 == 1 || e2 == -1)); // e1*e2 != 0
 	return e1*sqrtf(2)*sinf(pi/4.0f + (e1*e2)*alpha);
 }
 
 static float
 f_cross(int8_t e1, int8_t e2, float alpha) {
-	assert(e1*e2 == 0 && (e1+e2 == 1 || e1+e2 == -1));
+	assert(e1*e2 == 0 && (e1+e2 == 1 || e1+e2 == -1)); // e1*e1 == 0
 	const bool cond = e1 != 0;
 	return (e1+e2)*sinf(pi/2*cond + (cond ? -alpha : alpha));
 }
